a82720caa22dd7a8936a391e703e4c556c17098c
jmediane <jawad.mediane.ext@veolia.com>
2024-08-27 16:40:36 +0100

    Tests for products model

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase 
+from django.contrib.auth.models import User
 
 from store.models import Category, Product
 
@@ -16,3 +17,23 @@ class TestCategoriesModel( TestCase ):
         # Test Category model return name  
         data = self.data1
         self.assertEqual( str(data) , 'django' )
+
+
+class TestProductsModel( TestCase ):
+
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(
+            category_id=1, 
+            title='django beginners', 
+            created_by_id=1, 
+            slug='django-beginners', 
+            price='20.00', 
+            image='django'
+        )
+
+    def test_product_model_entry(self):
+        # Test Product model data insertion/types/field attributes 
+        data = self.data1
+        self.assertTrue( isinstance(data, Product) )
